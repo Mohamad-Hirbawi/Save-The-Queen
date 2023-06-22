760d6a557cc24ec812eb22e102875f272bb16555
mustafa fakhouri <abulila411@gmail.com>
2023-06-22 09:31:05 +0300

    we have the beastMonseter in last levels

--- src/Prince.cpp
+++ src/Prince.cpp
@@ -1,4 +1,4 @@
-#include "Prince.h"
+﻿#include "Prince.h"
 #include "GameController.h"
 
 Prince::Prince(Toolbar_t symbol, sf::Vector2f position)
@@ -7,9 +7,9 @@ Prince::Prince(Toolbar_t symbol, sf::Vector2f position)
     m_positionPrince = position;
 }
 
-void Prince::handleCollision(Stair& , GameController&)
+void Prince::handleCollision(Stair&s , GameController&)
 {
-	m_princeCollisStair = true;
+    m_princeCollisStair = true;
 }
 
 void Prince::handleCollision(Object& obj, GameController& game)
@@ -66,18 +66,124 @@ sf::Vector2f dirFromKey()
 }
 
 
+//void Prince::move(sf::Time deltaTime, sf::Vector2f)
+//{
+//    
+//    m_prevPos = m_icon.getPosition();
+//    sf::Vector2f dir = dirFromKey();
+//    if ((dir == RIGHT || dir == LEFT) )
+//    {
+//        m_icon.move(dir * MOVEMENTSPEED * deltaTime.asSeconds());
+//        m_dirPrince = dir;
+//    }
+//    else if (m_princeCollisStair)    m_icon.move(dir * MOVEMENTSPEED * deltaTime.asSeconds());
+//     
+//    m_princeCollisStair = false;
+//    m_positionPrince = m_icon.getPosition();
+//}
+//
+//
+
+//void Prince::move(sf::Time deltaTime, sf::Vector2f)
+//{
+//    m_prevPos = m_icon.getPosition();
+//    sf::Vector2f dir = dirFromKey();
+//
+//    if (m_princeCollisStair) {
+//        // إذا كان اللاعب في اتصال بالسلم
+//        if (dir == UP ||dir == LEFT|| dir == RIGHT ) {
+//            // الصعود على السلم
+//            m_icon.move(dir * MOVEMENTSPEED * deltaTime.asSeconds());
+//            m_dirPrince = dir;
+//        }
+//        
+//    }
+//    else {
+//        // إذا لم يكن اللاعب في اتصال بالسلم
+//        if (dir == RIGHT || dir == LEFT) {
+//            m_icon.move(dir * MOVEMENTSPEED * deltaTime.asSeconds());
+//            m_dirPrince = dir;
+//        }
+//    }
+//    if (dir == DOWN) {
+//    // النزول من السلم
+//        if (m_currentStair->isBottomCollision(m_icon)) {
+//            // إذا كان اللاعب قد وصل إلى أسفل السلم
+//            m_icon.move(dir * MOVEMENTSPEED * deltaTime.asSeconds());
+//            m_dirPrince = dir;
+//        }
+//    }
+//    m_princeCollisStair = false;
+//    m_positionPrince = m_icon.getPosition();
+//}
+//void Prince::move(sf::Time deltaTime, sf::Vector2f)
+//{
+//    m_prevPos = m_icon.getPosition();
+//    sf::Vector2f dir = dirFromKey();
+//
+//    if (m_princeCollisStair) {
+//        // إذا كان اللاعب في اتصال بالسلم
+//        //if (dir == UP || dir == LEFT || dir == RIGHT) {
+//            // الصعود على السلم
+//            m_icon.move(dir * MOVEMENTSPEED * deltaTime.asSeconds());
+//            m_dirPrince = dir;
+//        //}
+//    }
+//    else {
+//        // إذا لم يكن اللاعب في اتصال بالسلم
+//        if (dir == RIGHT || dir == LEFT) {
+//            m_icon.move(dir * MOVEMENTSPEED * deltaTime.asSeconds());
+//            m_dirPrince = dir;
+//        }
+//    }
+//
+//    if (dir == DOWN && !m_princeCollisStair) {
+//        // النزول من السلم
+//        if (m_currentStair->isBottomCollision(m_icon)) {
+//            // إذا كان اللاعب قد وصل إلى أسفل السلم
+//            m_icon.move(dir * MOVEMENTSPEED * deltaTime.asSeconds());
+//            m_dirPrince = dir;
+//        }
+//    }
+//
+//    m_princeCollisStair = false;
+//    m_positionPrince = m_icon.getPosition();
+//}
+
 void Prince::move(sf::Time deltaTime, sf::Vector2f)
 {
-    
     m_prevPos = m_icon.getPosition();
     sf::Vector2f dir = dirFromKey();
-    if ((dir == RIGHT || dir == LEFT) )
-    {
-        m_icon.move(dir * MOVEMENTSPEED * deltaTime.asSeconds());
-        m_dirPrince = dir;
+    //sf::FloatRect playerRect = m_icon.getGlobalBounds(); // المستطيل الخاص باللاعب
+    //playerRect.height -= 10; // المستطيل الخاص باللاعب
+
+    if (m_princeCollisStair) {
+        // إذا كان اللاعب في اتصال بالسلم
+       //if (dir == UP || dir == LEFT || dir == RIGHT) {
+            // الصعود على السلم
+            m_icon.move(dir * MOVEMENTSPEED * deltaTime.asSeconds());
+            m_dirPrince = dir;
+        //}
     }
-    else if (m_princeCollisStair)    m_icon.move(dir * MOVEMENTSPEED * deltaTime.asSeconds());
-     
+    else {
+        // إذا لم يكن اللاعب في اتصال بالسلم
+        if (dir == RIGHT || dir == LEFT) {
+            m_icon.move(dir * MOVEMENTSPEED * deltaTime.asSeconds());
+            m_dirPrince = dir;
+        }
+
+        //else if (dir == DOWN && playerRect.intersects(m_currentStair->getGlobalBounds())) {
+
+        //    // التحقق من وجود السلم أسفل اللاعب
+        //    if (playerRect.intersects(m_currentStair->getGlobalBounds())) {
+        //        // النزول على السلم
+        //        m_icon.move(dir * MOVEMENTSPEED * deltaTime.asSeconds());
+        //        m_dirPrince = dir;
+        //    }
+        //}
+    }
+
     m_princeCollisStair = false;
     m_positionPrince = m_icon.getPosition();
 }
+
