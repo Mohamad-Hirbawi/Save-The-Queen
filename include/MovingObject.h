12f38cd804dbd2d0d47d8e5217df9057920322a5
Mohammad Hirbawi <hirbawi198@gmail.com>
2023-06-22 19:14:10 +0300

    modolary6

--- include/MovingObject.h
+++ include/MovingObject.h
@@ -10,19 +10,8 @@ public:
 	//void moveToPrevPos();
 
 	virtual void move(sf::Time deltaTime, sf::Vector2f) = 0;
+	virtual void handleCollision(Object&, GameController&) {}
 
-	virtual void handleCollision(Object&, GameController&) override {};
-	virtual void handleCollision(Prince&, GameController&) override {};
-	virtual void handleCollision(Queen&, GameController&) override {};
-	virtual void handleCollision(Wall&, GameController&) override {};
-	virtual void handleCollision(Stair&, GameController&) override {};
-	virtual void handleCollision(Coin&, GameController&) override {};
-	virtual void handleCollision(Door&, GameController&) override {};
-	virtual void handleCollision(KeyMonster&, GameController&) override {};
-	virtual void handleCollision(BallMonster&, GameController&) override {};
-	virtual	void handleCollision(BeastMonster&, GameController&) override {};
-	virtual void handleCollision(Bullet&, GameController&) override {};
-	virtual void handleCollision(Gift&, GameController&) override {};
 
 	sf::Vector2f m_dirPrince;
 
